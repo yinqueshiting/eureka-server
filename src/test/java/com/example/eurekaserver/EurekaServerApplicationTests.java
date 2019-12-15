daded6714d45faf7771c4e88fd757e27be598530
给我红发一个面子 <yuanzihao147@foxmail.com>
2019-12-15 19:53:44 +0800

    修改本地冲突

--- src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.eurekaserver;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+public class EurekaServerApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
